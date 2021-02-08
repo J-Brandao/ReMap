30bb80f116e83b3d1236263265d2efb9a7925f3e
João Rangel <Jrangel29@ua.pt>
2021-02-08 19:17:48 +0000

    Página Login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Homepage from './Pages/Homepage';
-import './App.css';
+import './Styles/General.css';
 
 function App() {
   return (
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import '../Styles/Homepage.css'
+import styled from 'styled-components';
+import Logo from '../Images/Logo.png';
+import Background from '../Images/Background.png'
+
+const Home = styled.div`
+    flex-grow: 1;
+    background-image: url(${Background});
+    background-repeat: no-repeat;
+    background-size: 100% auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 100vh;
+`;
 
 function Homepage() {
     return(
-        <div>
-            ola maltinha
-        </div>
+        <Home>
+            <img src={Logo}/>
+            <h2>Bem-vindo ao Remap</h2>
+            <button className="btn login">Log In</button>
+            <button className="btn signup">Sign Up</button>
+        </Home>
     )
 }
 
