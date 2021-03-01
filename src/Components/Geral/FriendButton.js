775ed2b778c2fc2bce3e394b121e1e955158551a
J-Brandao <jp.brandao@ua.pt>
2021-03-01 15:55:49 +0000

    FriendButton 75% feito

--- src/Components/Geral/FriendButton.js
+++ src/Components/Geral/FriendButton.js
@@ -1,42 +1,60 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {createFriend, getFriends, deleteFriend} from '../../Store/Friends/Actions'
 import styled from "styled-components"
 import { ReactComponent as CheckMark } from "../../Images/CheckMark.svg"
-import { ReactComponent as AddFriend} from "../../Images/AdicionarAmigo.svg"
+import { ReactComponent as AddFriend } from "../../Images/AdicionarAmigo.svg"
+import {fetchFriends} from "../../Firebase/Pedidos"
 
-function FriendButton({friendId, userId, friendName, imageFriend}) {
-const dispatch = useDispatch()
-  const isFriend = useSelector(({ Friends }) => Friends.data)
-  const isLoadingFriendship = useSelector(({ Friends }) => Friends.isLoadingFriendship)
+function FriendButton({ friendId, userId, friendName, imageFriend }) {
+
+  const dispatch = useDispatch()
+  const [isFriend, setIsFriend] = useState(false); 
+  const [isLoading, setIsLoading] = useState(true); 
   
-  
-  useEffect(() => {
-    dispatch(getFriends(userId, friendId)) 
-  }, [dispatch, userId, friendId])
+  async function fetch(userId, friendId) {
+      const response = await fetchFriends(userId, friendId);  
+      setIsFriend(response)
+      setIsLoading(false)
+    }
+
+  useEffect(() => { 
+    if (isLoading) {
+      fetch(userId, friendId)
+    }
+    if (!isLoading)
+      console.log(isFriend)
+  }, [friendId, userId, isLoading])
 
   const onCreateFriend = (friendName, userId, friendId, imageFriend) => {
+     setIsLoading(true);
     dispatch(createFriend(friendName, userId, friendId, imageFriend))
+   
+    
   }
 
   const onDeleteFriend = (id) => {
+    setIsLoading(true);
     dispatch(deleteFriend(id))
+   
+    
   }
 
-  if (isLoadingFriendship) {
+ 
+  if (isLoading) {
     return null
   }
   
   
   return (
-    <>
-      
-      {isFriend ?
+    <>{isFriend ?
         <Friend onClick={() => onDeleteFriend(isFriend.id)}><CheckMark width="30px" /></Friend>
         :
         <Friend onClick={() => onCreateFriend(friendName, userId, friendId, imageFriend)}><AddFriend width="30px" /></Friend>
 
+    
     }
+      
     </>
   )
 }
